ad4359fa567114cd3a26f6434e2ad9b17a266e9a
Uzeyir Mammadov <uzeyirmammad@gmail.com>
2021-05-18 19:59:44 +0200

    Delete console.log from forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,6 @@ const forecast = (lat, long, callback) => {
         } else if (body.error){
             callback('Location not found!', undefined)
         } else{
-            console.log(body.current.weather_descriptions[0] + body.current.humidity)
             callback(undefined, body.current.weather_descriptions[0] +" right now. "+"Humidity is "+ body.current.humidity +" percent. "+"It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike  + " degrees out")
         
        }
